62428f189d9a38bc28a773ec3c5fea0ca8395c89
Jesse <JesseBefus@hotmail.com>
2018-05-16 12:50:17 -0600

    Update server.lua
    Added new variable and allowed for multiple fines.

--- server.lua
+++ server.lua
@@ -3,9 +3,21 @@ ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterServerEvent('fineAmount')
-AddEventHandler('fineAmount', function()
+AddEventHandler('fineAmount', function(mphspeed)
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(source)
+	local truespeed = mphspeed
+	if truespeed >= 55 and truespeed <= 65 then
 	xPlayer.removeMoney(Config.Fine)
+	end
+	if truespeed >= 65 and truespeed <= 75 then
+	xPlayer.removeMoney(Config.Fine2)
+	end
+	if truespeed >= 75 and truespeed <= 85 then
+	xPlayer.removeMoney(Config.Fine3)
+	end
+	if truespeed >= 85 and truespeed <= 200 then
+	xPlayer.removeMoney(Config.Fine4)
+	end
 	CancelEvent()
 end)
